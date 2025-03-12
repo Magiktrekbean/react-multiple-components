a5dba053c1c0613ba83b38135c3ab1bdc2db54d7
Magiktrekbean <153703739+Magiktrekbean@users.noreply.github.com>
2025-03-11 22:46:40 -0400

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import react from "react";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <body>Contact us to add more cities</body>
+    </div>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import react from "react";
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className="Header">
+      <body>The list of cities is:</body>
+    </div>
+  );
+}
